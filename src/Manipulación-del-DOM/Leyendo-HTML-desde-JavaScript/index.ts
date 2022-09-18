656c6686f0bab1977c34b2706520687f3deb9a62
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-09-18 20:01:51 -0300

    :white_check_mark: Clase N°10: Leyendo HTML desde JavaScript.

---
+++ src/Manipulación-del-DOM/Leyendo-HTML-desde-JavaScript/index.ts
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1') as HTMLHeadElement;
+const p = document.querySelector('p') as HTMLParagraphElement;
+const parrafito = document.querySelector('.parrafito') as HTMLParagraphElement;
+const pid = document.querySelector('#pid') as HTMLParagraphElement;
+const input = document.querySelector('input') as HTMLInputElement;
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+});
+
+console.log(input.value);
